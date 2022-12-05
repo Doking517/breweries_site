27c5f51f529393cb28b10f5230762c6ad0601e81
doking517 <tsemojust@gmail.com>
2022-12-06 00:36:44 +0100

    fetching data from the API

--- src/components/BrowseOurMenu/BrowseOurMenu.jsx
+++ src/components/BrowseOurMenu/BrowseOurMenu.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 const BrowseOurMenu = () => {
+	const [breweries, setBreweries] = useState(null);
+	let apiURL = 'https://api.openbrewerydb.org/breweries?per_page=50';
+
+	useEffect(() => {
+		fetch(apiURL)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setBreweries(data);
+				console.log(breweries);
+			});
+	}, []);
+
 	return (
 		<div>
 			<h1 className="text-red-500">Browse our menu</h1>
